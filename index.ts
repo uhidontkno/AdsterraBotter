ba94537710dba8bfd482f6b5d6a8ce42c35264f8
rare1k <hi@rare1k.dev>
2024-06-11 10:55:37 -0400

    feat: get selenium working

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+function timeout(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+import * as s from "selenium-webdriver"
+let driver = await new s.Builder().forBrowser(s.Browser.CHROME).build()
+  try {
+    await driver.get('https://www.google.com/ncr')
+    await driver.findElement(s.By.name('q')).sendKeys('webdriver', s.Key.RETURN)
+    await driver.wait(s.until.titleIs('webdriver - Google Search'), 1000)
+    await timeout(1000)
+  } finally {
+    await driver.quit()
+  }
